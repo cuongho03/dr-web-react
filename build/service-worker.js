12bed53ce2dcb2e4bf19808c9c71de0ae5e88340
quốc cường hồ <cuongseven789@gmail.com>
2020-06-27 22:30:12 +0700

    [U] git push

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dfa3561102faead6162946d67b91f409.js"
+  "/precache-manifest.b19e795b5f805deeac81d91f8295e6ee.js"
 );
 
 self.addEventListener('message', (event) => {
